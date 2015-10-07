029811c9e480440725289343039df2efdadfafee
root <root@ip-10-234-17-43.ec2.internal>
2015-10-07 00:16:08 +0000

    Minor

--- export.sh
+++ export.sh
@@ -1,24 +1,24 @@
 export _JAVA_OPTIONS=-Xmx70G
-jar='/export/home/mraza/Rwanda_DailyCOG/target/scala-2.10/dailycogmain_2.10-0.1.jar'
-file_suffix='-Mobility.sor.txt'
-hadoop_outdir_prefix='/user/mraza/Rwanda_Out/RawMobilityFiles/'
-hadoop_outdir_suffix='*/part-*'
+hadoop_path='~/ephemeral-hdfs/bin'
+hadoop_indir_prefix='Rwanda_In/RawMobilityFiles/'
+hadoop_indir_suffix='-Mobility.sor.txt-DailyEveningCOG/part-*'
 
-output_path='/export/home/mraza/Rwanda_Output/HourlyCOG/'
-output_suffix='_HourlyCOG'
+output_path='./'
+output_suffix='_HourlyEveningCOG'
 
 exec_obj_name='DailyCOGMain'
 
 export SPARK_JAVA_OPTS+=" -verbose:gc -Xmx15g -Xms1g -XX:-PrintGCDetails -XX:+PrintGCTimeStamps "
 
 #for month in   0703
-for month in    0801 0802   0804 0805
+for month in    0604
 do
 #        echo "Trying jar $jar file $month$file_suffix ";
 
 #        spark-submit  --class $exec_obj_name --master yarn-client $jar $month$file_suffix --verbose;
-#       echo "Executing export hadoop fs -cat $hadoop_outdir_prefix$month$hadoop_outdir_suffix>>$output_path$month$output_suffix "
+       echo "Executing export hadoop fs -cat ~/ephemeral-hdfs/bin/hadoop fs -cat $hadoop_indir_prefix$month$hadoop_indir_suffix "
 
-       hadoop fs -cat $hadoop_outdir_prefix$month$hadoop_outdir_suffix>>$output_path$month$output_suffix;
+       #$hadoop_path/hadoop fs -cat $hadoop_indir_prefix$month$hadoop_indir_suffix>>$output_path$month$output_suffix;
+       ~/ephemeral-hdfs/bin/hadoop fs -cat $hadoop_indir_prefix$month$hadoop_indir_suffix>>$output_path$month$output_suffix;
 done
 
